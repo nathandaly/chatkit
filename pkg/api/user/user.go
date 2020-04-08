cba79453d4d1cc78369b5f8f054ac7d59055747f
Nathan Daly <nathan.daly@toolboxmarketing.com>
2020-04-08 11:00:19 +0100

    Adding user create endpoint and function without persistence currently.

--- pkg/api/user/user.go
+++ pkg/api/user/user.go
@@ -1,7 +1,7 @@
 package user
 
 import (
-	"fmt"
+	"encoding/json"
 	"github.com/gofiber/fiber"
 	"time"
 )
@@ -10,10 +10,10 @@ type User struct {
 	Id string `json:"id"`
 	Name string `json:"name"`
 	AvatarUrl string `json:"avatar_url"`
-	CustomData string `json:"custom_data"`
+	CustomData map[string]interface{} `json:"custom_data"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
-	DeletedAt time.Time `json:"deleted_at"`
+	DeletedAt *time.Time `json:"deleted_at"`
 }
 
 func Routes(group *fiber.Group) {
@@ -23,7 +23,21 @@ func Routes(group *fiber.Group) {
 }
 
 func CreateAUser(c *fiber.Ctx) {
-	c.Send("Hello, World!" + c.Params("name"))
-	fmt.Printf("Hello %s!", c.Params("name"))
-	// => Hello john!
+	var user User
+	var result map[string]interface{}
+
+	if err := json.Unmarshal([]byte(c.Body()), &result); err != nil {
+		c.Send("Error parsing body.")
+	}
+
+	if err := c.BodyParser(&user); err != nil {
+		c.Send("Error parsing user.")
+	}
+
+	user.CustomData = result["custom_data"].(map[string]interface{})
+	user.DeletedAt = nil
+
+	if err := c.JSON(user); err != nil {
+		c.Send("Error converting user to JSON.")
+	}
 }
