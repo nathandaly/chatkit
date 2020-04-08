97b8746b11c7309a17700f9035b250ba7e148649
Nathan Daly <nathan.daly@toolboxmarketing.com>
2020-04-08 01:45:24 +0100

    Removed line.

--- pkg/api/user/user.go
+++ pkg/api/user/user.go
@@ -3,14 +3,27 @@ package user
 import (
 	"fmt"
 	"github.com/gofiber/fiber"
+	"time"
 )
+
+type User struct {
+	Id string `json:"id"`
+	Name string `json:"name"`
+	AvatarUrl string `json:"avatar_url"`
+	CustomData string `json:"custom_data"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	DeletedAt time.Time `json:"deleted_at"`
+}
 
 func Routes(group *fiber.Group) {
 	user := group.Group("/user")
 
-	user.Get("/:name", func(c *fiber.Ctx) {
-		c.Send("Hello, World!" + c.Params("name"))
-		fmt.Printf("Hello %s!", c.Params("name"))
-		// => Hello john!
-	})
+	user.Post("/", CreateAUser)
+}
+
+func CreateAUser(c *fiber.Ctx) {
+	c.Send("Hello, World!" + c.Params("name"))
+	fmt.Printf("Hello %s!", c.Params("name"))
+	// => Hello john!
 }
